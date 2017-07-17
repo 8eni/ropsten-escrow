3b90804caa8482e61547739df6cb0c8c58286b97
Benni Russell <bennirus@Sarahs-MacBook-Pro.local>
2017-07-17 22:27:38 +0100

    updates UI to work with MetaMask

--- app/metacoin/metacoin.component.js
+++ app/metacoin/metacoin.component.js
@@ -1,115 +1,101 @@
 import metacoinHtml from './metacoin.html';
+import metacoinJson from './metacoin.json';
 import Web3 from 'web3';
-import util from 'ethereumjs-util';
-import tx from 'ethereumjs-tx';
-import lightwallet from 'eth-lightwallet';
 import jquery from 'jquery';
 import bootstrap from 'bootstrap';
-import EscrowAbiController from '../../build/contracts/EscrowAbiController.json';
 
 let metacoinComponent = {
 	template: metacoinHtml,
 	controllerAs: 'vm',
-	controller: function(metacoinService, $scope, $interval) {
+	controller: function(metacoinService, $scope, $interval, $timeout) {
 		
 		const vm = this;
-		var txutils = lightwallet.txutils;
 		vm.title = metacoinService.title();
 
-		var address = '0x85f4f57507d8eeEa0447F855866656F3DC10DDf2';
-		var address2 = '0xD2C8a7C6c14f185095AA1147D313b9be5a542041';
-		var key = '';
-		var key2 = '';
-
 		// Connect Web3 Instance
-		var web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/AyifF0rnvHClXpU46hu9"));
-		var contractAbi = [{"constant":true,"inputs":[],"name":"getContractBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"sendEthToContract","outputs":[{"name":"","type":"uint256"}],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"getChairpersonAddress","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"sendContractEthToChairperson","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}];
+		var web3Query = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/AyifF0rnvHClXpU46hu9"));
+		var contractAbi = metacoinJson;
 		var contractAddress = '0x9B78e854A6c18f7a2Db9736C9f5404568a9b8Da4';
 		var contract = web3.eth.contract(contractAbi).at(contractAddress);
-
-		//
-		var sendRaw = (rawTx) => {
-		  var privateKey = new Buffer(key, 'hex');
-		  var transaction = new tx(rawTx);
-		  transaction.sign(privateKey);
-		  var serializedTx = transaction.serialize().toString('hex');
-		  web3.eth.sendRawTransaction(
-		  '0x' + serializedTx, function(err, result) {
-		    if(err) {
-		        console.log(err);
-		    } else {
-		        console.log('success',result);
-		        pending(result);
-		    }
-		  });
-		}		
 		
 		var pending = (txn) => {
-			vm.chairpersonAddress = 'Pending TXN';
+			vm.status = 'Pending TXN';
 			vm.currentTxn = txn;
 			vm.linkTxn = `https://ropsten.etherscan.io/tx/${txn}`;
-			var pendInt;
-			pendInt = $interval(()=>{
-				if (!web3.eth.getTransactionReceipt(txn)) {
+			var pendingInterval;
+			pendingInterval = $interval(()=>{
+				if (!web3Query.eth.getTransactionReceipt(txn)) {
 					console.log('pending...')	
 				} else {
-					vm.chairpersonAddress = 'TXN in blockchain';
-					$interval.cancel(pendInt);
+					vm.status = 'TXN in blockchain';
+					$timeout(()=>{
+						getContractBalance();
+					},1000)
+					$interval.cancel(pendingInterval);
 				}
 			},1000)
 		}
 
-		// Sending TXN
-		var txOptions = {
-		    nonce: web3.toHex(web3.eth.getTransactionCount(address)),
-		    gasLimit: web3.toHex(80000),
-		    gasPrice: web3.toHex(20000000000),
-		    to: contractAddress
-		}
-		var txOptionsWithValue = {
-		    nonce: web3.toHex(web3.eth.getTransactionCount(address)),
-		    gasLimit: web3.toHex(80000),
-		    gasPrice: web3.toHex(20000000000),
-		    value: 110000000000000000,// web3.toWei(.11, 'ether'),
-		    to: contractAddress
+		var init = () => {
+			getChairpersonAddress();
+			getContractBalance();
 		}
 		
-		var sendEthToContract = txutils.functionTx(contractAbi, 'sendEthToContract', [], txOptionsWithValue);
-		var sendContractEthToChairperson = txutils.functionTx(contractAbi, 'sendContractEthToChairperson', [], txOptions);
-
 		// SETTERS
 		vm.sendEthToContract = () => {
-			console.log('txOptionsWithValue',txOptionsWithValue)
-			sendRaw(sendEthToContract);
+			contract.sendEthToContract.sendTransaction({
+        from: web3.eth.accounts[0],
+        value: web3.toWei(.01, 'ether')
+      }, function(err, result) {
+        if (!err) {
+        	pending(result);
+        } else {
+          console.log(err);
+        }
+      })
 		}
 
 		vm.sendContractEthToChairperson = () => {
-			console.log('txOptions',txOptions)
-			sendRaw(sendContractEthToChairperson)
+			contract.sendContractEthToChairperson.sendTransaction({
+        from: web3.eth.accounts[0]
+      }, function(err, result) {
+        if (!err) {
+        	pending(result);
+        } else {
+          console.log(err);
+        }
+      })
 		}
+
 		
 		// GETTERS
-		vm.getChairpersonAddress = () => {
+		var getChairpersonAddress = () => {
 			contract.getChairpersonAddress.call((err, result) => {
-				if(err) {
-					console.log(err);
-				} else {
-					console.log(result);
+				if(!err) {
+					// console.log(result)
 					vm.chairpersonAddress = result;
-					$scope.$apply();
-				}
-			});
-		}
-		vm.getContractBalance = () => {
-			contract.getContractBalance.call((err, result) => {
-				if(err) {
-					console.log(err);
 				} else {
-					console.log(web3.fromWei(result, 'ether').toNumber())
-					console.log(result);
+					// console.log(err);
+					vm.chairpersonAddress = err;
 				}
+				$scope.$apply();
 			});
 		}
+
+		var getContractBalance = () => {
+			contract.getContractBalance.call((err, result) => {
+				if(!err) {
+					// console.log(web3.fromWei(result, 'ether').toNumber());
+					vm.contractBalance = `${web3.fromWei(result, 'ether').toNumber()} ETH`;
+				} else {
+					// console.log(err);
+					vm.contractBalance = err;
+				}
+				$scope.$apply();
+			});
+		}
+
+		init();
 
 	}
 
